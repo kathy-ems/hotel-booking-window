d80d08dadc23a4c240828c629285bd6544b90d93
Kathy Ems <kathy-ems@users.noreply.github.com>
2023-03-09 11:31:06 -0800

    checks for date and sends email

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # hotel-booking-window
 Bot to determine if a booking window has opened
+
+
+## Getting Started
+Install python 3.9
+
+Create virtual environment .venv
+
+
+```
+$ python3.9 -m venv .venv
+$ poetry config virtualenvs.in-project true
+$ poetry install
+$ pip install python-dotenv
+
+```
+
+## To Run Application
+Set all the params for creating a tee time in the top of `main.py` then run the below commands
+
+
+```
+$ source ./.venv/bin/activate
+$ set -o allexport; source .env; set +o allexport
+$ python3 main.py
+```
+
+
+### Troubleshooting
+Error: Selenium modual is not installed
+
+```
+$ source ./.venv/bin/activate
+$ pip3 uninstall selenium
+$ pip3 install selenium
+$ echo $VIRTUAL_ENV
+$ source activate $VIRTUAL_ENV
+```
---
+++ main.py
@@ -0,0 +1,93 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import smtplib
+import os
+from dotenv import load_dotenv
+from email.message import EmailMessage
+
+# Set up the email parameters
+EMAIL_ADDRESS = os.environ.get('EMAIL')
+EMAIL_PASSWORD = os.environ.get('PASSWORD')
+sender_email = os.environ.get('SENDER_EMAIL')
+recipient_email = EMAIL_ADDRESS
+
+# create the email message object
+msg = EmailMessage()
+msg['From'] = sender_email
+msg['To'] = recipient_email
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    print('Logged in successfully')
+    try:
+        # Set up the Chrome driver
+        driver = webdriver.Chrome()
+
+        # Navigate to the Marriott homepage
+        driver.get("https://www.marriott.com/default.mi")
+
+        # Wait for the page to load
+        wait = WebDriverWait(driver, 5)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "m-header__logo-icon")))
+
+        # Find the check-in date input field and enter the desired date (April 8, 2023)
+        checkin_field = driver.find_element(By.CLASS_NAME, "search__calendar-value")
+        checkin_field.click()
+        def find_desired_month(driver, desired_month):
+            global furthest_booking_month
+            months_in_view = driver.find_elements(By.CLASS_NAME, "DayPicker-Caption")
+            for month in months_in_view:
+                if desired_month in month.text.upper():
+                    print("Found the desired month:", desired_month)
+                    return True
+            else:
+                furthest_booking_month = month.text
+                # if not correct month, navigate to next month
+                month_picker = driver.find_element(By.CLASS_NAME, "DayPicker-NavButton--next")
+                try:
+                    WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "DayPicker-NavButton--next")))
+                    month_picker.click()
+                    return False
+                except:
+                    print("Month picker is not found. Breaking out of the loop.")
+                    return True
+
+        desired_month = "APRIL 2024"
+        while not find_desired_month(driver, desired_month):
+            pass
+
+        # See if desired Date is available (April 8, 2024)
+        desired_date = "Mon Apr 02 2024"
+        date_elements = driver.find_elements(By.CSS_SELECTOR, 'div[aria-label]')
+        print(furthest_booking_month)
+        # loop through each date element and check if it matches the desired date
+        for date_element in date_elements:
+            date_string = date_element.get_attribute('aria-label')
+            if date_string == desired_date:
+                msg['Subject'] = 'Marriott Booking Window IS Open!!!!'
+                msg.set_content(f'Booking window is open for {desired_date}')
+                print("Booking window is open! ", desired_date)
+                server.send_message(msg)
+                break
+        else:
+            msg['Subject'] = 'Marriott Booking Window Not Open'
+            msg.set_content(f'Booking month is {furthest_booking_month}. Window has not opened yet for {desired_date}')
+            print(f'Booking month is {furthest_booking_month}. Window has not opened yet for {desired_date}')
+            server.send_message(msg)
+
+    except Exception as e:
+        print(f'Error booking!: {e}')
+        server.sendmail(sender_email, recipient_email, f'Error booking: {e}')
+
+    # Close the webdriver and email server
+    driver.quit()
+    server.quit()
+except Exception as e:
+    print(f'Error with gmail server: {e}')
+
